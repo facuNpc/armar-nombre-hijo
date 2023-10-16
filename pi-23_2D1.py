c5063069e997e01be485afcf70cbc60c572c862b
Facuindo <facu_nicogas@outlook.com>
2023-10-16 07:56:28 -0300

    inputs

---
+++ pi-23_2D1.py
@@ -0,0 +1,8 @@
+
+# Empezar aquí la resolución del ejercicio
+nombre_completo_1 = str(input("nombre completo del padre,madre o tutor/a: "))
+
+nombre_completo_2 = str(input("nombre completo del padre,madre o tutor/a: "))
+
+nombre = str(input("Nombre del hijo/a: "))
+
