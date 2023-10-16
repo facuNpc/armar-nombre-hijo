dc6c256c226b40064d7e68a3711479bd09cd7272
Facuindo <facu_nicogas@outlook.com>
2023-10-16 07:58:12 -0300

    split y armado de nombre final

--- pi-23_2D1.py
+++ pi-23_2D1.py
@@ -1,8 +1,10 @@
 
 # Empezar aquí la resolución del ejercicio
 nombre_completo_1 = str(input("nombre completo del padre,madre o tutor/a: "))
-
 nombre_completo_2 = str(input("nombre completo del padre,madre o tutor/a: "))
-
 nombre = str(input("Nombre del hijo/a: "))
 
+nombre_1, apellido_1 = nombre_completo_1.split(" ")
+nombre_2, apellido_2 = nombre_completo_2.split(" ")
+hijo = nombre + " " + apellido_1 + " " + apellido_2
+print(hijo)
